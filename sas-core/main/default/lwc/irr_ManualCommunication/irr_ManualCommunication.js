8e0d7656603d475fa3454fb70fba286de54261bd
Niklas Lundkvist <nlundkvist@deloitte.se>
2020-05-19 16:01:10 +0200

    Template Message - Updated lwc, classes, tests, flows and custom metadata to use Name (Label) instead of DeveloperName for templates

--- sas-core/main/default/lwc/irr_ManualCommunication/irr_ManualCommunication.js
+++ sas-core/main/default/lwc/irr_ManualCommunication/irr_ManualCommunication.js
@@ -224,6 +224,8 @@ export default class IRR_ManualCommunication extends LightningElement {
                 case "DELAY":
                     payload.delayInfo = parameters;
                     break;
+                case "TEMPLATE":
+                    break;
                 default:
                     return;
             }
