2dc5cba90ac5fa545e7389349973ff177bd59ca8
Mikko Karjalainen <mikko.karjalainen@columbiaroad.com>
2021-02-15 18:40:34 +0100

    Fix also chat page tagging

--- sas-core/main/default/lwc/fcs_CaseTag_ChatTranscript/fcs_CaseTag_ChatTranscript.js
+++ sas-core/main/default/lwc/fcs_CaseTag_ChatTranscript/fcs_CaseTag_ChatTranscript.js
@@ -1,7 +1,7 @@
-import {LightningElement, api, track, wire } from 'lwc';
-import { ShowToastEvent} from 'lightning/platformShowToastEvent';
+import { LightningElement, api, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import findCase from '@salesforce/apex/FCS_CaseTag_ChatTranscript_Controller.findCase';
-var checkClick;
+
 export default class Fcs_CaseTag extends LightningElement {
     @api recordId;
     @track cse;
@@ -22,32 +22,14 @@ export default class Fcs_CaseTag extends LightningElement {
             error = 'error occured';
         }
     }
-    handleSubmit(event) {
-        checkClick = 'submit';
-        const allValid = [...this.template.querySelectorAll('lightning-input-field')]
-            .reduce((validSoFar, inputCmp) => {
-                inputCmp.reportValidity();
-                return validSoFar && inputCmp.checkValidity();
-            }, true);
-        if (allValid) {
-            this.template.querySelector('lightning-record-edit-form').submit(fields);
-        }
-    }
-    handleSubmitOnKeyPress(event) {
-        if (event.altKey) {
-            checkClick = 'submit';
-            this.template.querySelector('lightning-record-edit-form').submit(fields);
-        }
-    }
+
     handleSuccess(event) {
-        if (checkClick == "submit") {
-            const evt = new ShowToastEvent({
-                title: 'Success',
-                message: 'Submitted Successfully',
-                variant: 'success'
-            });
-            this.dispatchEvent(evt);
-        }
+        const evt = new ShowToastEvent({
+            title: 'Success',
+            message: 'Submitted Successfully',
+            variant: 'success'
+        });
+        this.dispatchEvent(evt);
     }
     handleReset(event) {
         checkClick = 'reset';
