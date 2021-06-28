7a08ca521c2f92284c054081588ff24150c5e217
Mikko Karjalainen <mikko.karjalainen@columbiaroad.com>
2021-06-28 09:15:12 +0200

    Fix customer card record type check

--- sas-core/main/default/lwc/customerCard/customerCard.js
+++ sas-core/main/default/lwc/customerCard/customerCard.js
@@ -27,7 +27,6 @@ export default class CustomerCard extends LightningElement {
   @track cardTitle = "";
   accountId = undefined;
   caseIdForChats = undefined;
-  isChatTranscript = this.objectApiName === "LiveChatTranscript";
 
   // UI state
   @track showSpinner = false;
@@ -62,7 +61,7 @@ export default class CustomerCard extends LightningElement {
       } else {
         this.accountId = data.fields.AccountId.value;
       }
-      if (this.isChatTranscript) {
+      if (this.objectApiName === "LiveChatTranscript") {
         this.caseIdForChats = data.fields.CaseId.value;
       }
     } else {
@@ -140,7 +139,7 @@ export default class CustomerCard extends LightningElement {
       });
       if (account) {
         try {
-          if (this.isChatTranscript) {
+          if (this.objectApiName === "LiveChatTranscript") {
             console.log("#addCustomerFromCase.chatTranscript");
             const caseRecordInput = {
               fields: {
@@ -187,7 +186,7 @@ export default class CustomerCard extends LightningElement {
     console.log("#removeCustomerToCase.start");
     this.showSpinner = true;
     try {
-      if (this.isChatTranscript) {
+      if (this.objectApiName === "LiveChatTranscript") {
         console.log("#removeCustomerFromCase.chatTranscript");
         const caseRecordInput = {
           fields: {
