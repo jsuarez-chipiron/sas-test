91468903a4bd76e75e0fb655fd4785943741f4fc
thippeswamyaravinda <thippeswamy.aravinda@sas.dk>
2021-07-26 17:32:02 +0530

    Case Tag rework

--- sas-core/main/default/lwc/fcs_CaseTag_ChatTranscript/fcs_CaseTag_ChatTranscript.js
+++ sas-core/main/default/lwc/fcs_CaseTag_ChatTranscript/fcs_CaseTag_ChatTranscript.js
@@ -1,11 +1,16 @@
 import { LightningElement, api, track, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import findCase from "@salesforce/apex/FCS_CaseTag_ChatTranscript_Controller.findCase";
+import findCase from "@salesforce/apex/FCS_CaseTag_Controller.findCase";
+import findCaseTag from "@salesforce/apex/FCS_CaseTag_Controller.findCaseTag";
 export default class Fcs_CaseTag extends LightningElement {
   @api recordId;
   @track cse;
   @track error = undefined;
   @track RecordTypeId;
+  @track hideResolutionComment = false;
+  @track caseTagged = false;
+  @track caseTag;
+  @track reset = false;
   @wire(findCase, {
     recordId: "$recordId"
   })
@@ -23,16 +28,43 @@ export default class Fcs_CaseTag extends LightningElement {
       this.displayError(error);
     }
   }
+  @wire(findCaseTag, {
+    recordId: "$recordId"
+  })
+  wiredCaseTag({ error, data }) {
+    if (data) {
+      try {
+        this.caseTag = data;
+        this.caseTagged = true;
+      } catch (error) {
+        this.caseTag = undefined;
+      }
+    } else {
+      this.displayError(error);
+    }
+  }
+  handleDataChange(event) {
+    this.reason = event.target.value;
+    if (this.reason == "other") {
+      this.hideResolutionComment = true;
+    } else {
+      this.hideResolutionComment = false;
+    }
+  }
   displayError(error) {
     this.error = error;
   }
   handleSuccess(event) {
-    const evt = new ShowToastEvent({
-      title: "Success",
-      message: "Submitted Successfully",
-      variant: "success"
-    });
-    this.dispatchEvent(evt);
+    if (!this.reset) {
+      const evt = new ShowToastEvent({
+        title: "Success",
+        message: "Submitted Successfully",
+        variant: "success"
+      });
+      this.caseTagged = true;
+      this.dispatchEvent(evt);
+    }
+    this.reset = false;
   }
   handleReset(event) {
     const inputFields = this.template.querySelectorAll("lightning-input-field");
@@ -40,6 +72,9 @@ export default class Fcs_CaseTag extends LightningElement {
       inputFields.forEach((field) => {
         field.reset();
       });
+      if (this.caseTagged) {
+        this.reset = true;
+      }
     }
   }
 }
