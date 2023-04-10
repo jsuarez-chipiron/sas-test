eb6481b86b241a6a3f8fc7b250fae0fb42fd5416
Javier Suarez <jsuarez@salesforce.com>
2023-04-10 16:28:14 +0200

    deploy: 2

--- bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
+++ bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-./pmd check -R rulesets/apex/quickstart.xml -d ~/Projects/8_SAS/repos/sfdc/My_FCS_Salesforce/sas-core/main/default/classes/ -f csv 2> /dev/null > report.json
+./pmd check -R rulesets/apex/quickstart.xml -d ../../../../target/src/classes/ -f csv 2> /dev/null > report.csv
 if [ "$?" -ne 0 ]
 then
-    echo "Errors has beed detected"
-
-    cat report.json | awk -F',' '$4~1 {print $0}'
+    echo "Errors/Warnings has beed detected"
+    if [ $(cat report.csv | awk -F',' '$4~1 {print $0}' | wc -l) -ne 0 ]
+    then
+        cat report.csv | awk -F',' '$4~1 {print $0}'
+        exit 1
+    fi
 fi
 
