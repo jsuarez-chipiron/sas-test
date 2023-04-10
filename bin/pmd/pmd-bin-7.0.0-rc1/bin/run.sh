ca8519462f7a7b77be73e3bb17e02b5e59ca974a
Javier Suarez <jsuarez@salesforce.com>
2023-04-10 17:14:53 +0200

    update run.sh

--- bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
+++ bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-./pmd check -R rulesets/apex/quickstart.xml -d ../../../../target/src/classes/ -f csv 2> /dev/null > report.csv
+./pmd check -R rulesets/apex/quickstart.xml -d ../../../../target/src/classes/ -f csv 2> result.csv > result.csv
 if [ "$?" -ne 0 ]
 then
     echo "Errors/Warnings has beed detected"
-    cat report.csv | awk -F',' '$4~1 {print $0}' 
-    cat report.csv | awk -F',' '$4~1 {print $0}' | wc -l
-    if [ $(cat report.csv | awk -F',' '$4~1 {print $0}' | wc -l) -ne 0 ]
+    cat result.csv
+    cat result.csv | awk -F',' '$4~1 {print $0}' 
+    cat result.csv | awk -F',' '$4~1 {print $0}' | wc -l
+    if [ $(cat result.csv | awk -F',' '$4~1 {print $0}' | wc -l) -ne 0 ]
     then
         exit 1
     fi
