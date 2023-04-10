a1337832dc2fbe49bb7c865c87f170fefdd1e3ed
Javier Suarez <jsuarez@salesforce.com>
2023-04-10 16:31:22 +0200

    deploy: 2

--- bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
+++ bin/pmd/pmd-bin-7.0.0-rc1/bin/run.sh
@@ -4,9 +4,10 @@
 if [ "$?" -ne 0 ]
 then
     echo "Errors/Warnings has beed detected"
+    cat report.csv | awk -F',' '$4~1 {print $0}' 
+    cat report.csv | awk -F',' '$4~1 {print $0}' | wc -l
     if [ $(cat report.csv | awk -F',' '$4~1 {print $0}' | wc -l) -ne 0 ]
     then
-        cat report.csv | awk -F',' '$4~1 {print $0}'
         exit 1
     fi
 fi
